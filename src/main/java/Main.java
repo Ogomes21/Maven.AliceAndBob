36f14d8b552f3d4677ec3b7cf47a31acf66227ee
octavia <Ogomes21@gmail.com>
2022-07-03 16:45:22 -0400

    Greeted Alice & Bob. Denied others.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args ){
+    //Prompt user to input name
+    //Greet users who are Alice and Bob
+    public static void main(String[] args) {
+        String userInput; //= String.valueOf(new Scanner(""));
+        String out = "";
+
+        System.out.println("What is your name?");
+        Scanner input = new Scanner(System.in);
+        userInput=input.nextLine();
+
+        if (userInput.equalsIgnoreCase("Alice")) {
+            System.out.println("Hello, Alice!");
+
+        }
+       else if (userInput .equalsIgnoreCase("Bob")) {
+            System.out.println("Hello, Bob!");
+        }
+
+        else {
+            System.out.println("Sorry. Access Denied");
+        }
 
     }
 }
